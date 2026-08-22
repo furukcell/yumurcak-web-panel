7ba6f561fe6780bdad531a39d0101a72bdddbc05
furukcell <kurtulusfaruk94@gmail.com>
2026-08-22 08:02:04 +0300

    Create constants.js

---
+++ src/constants.js
@@ -0,0 +1,20 @@
+// Mobildeki src/constants.js'den web paneli için gereken sabitler.
+
+export const YAS_GRUPLARI = [
+  { key: '0-1', label: '0-1 Yaş' },
+  { key: '1-2', label: '1-2 Yaş' },
+  { key: '2-3', label: '2-3 Yaş' },
+  { key: '3-4', label: '3-4 Yaş' },
+  { key: '4-5', label: '4-5 Yaş' },
+  { key: '5-6', label: '5-6 Yaş' },
+  { key: 'karma', label: 'Karma (Farklı Yaşlar)' },
+];
+
+// bkz. mobil src/constants.js -> ROLLER
+export const ROLLER = {
+  SUPERADMIN: 'superadmin',
+  YONETICI: 'yonetici',
+  OGRETMEN: 'ogretmen',
+  VELI: 'veli',
+  SERVISCI: 'servisci',
+};
